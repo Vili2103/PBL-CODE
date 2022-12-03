5f04d7a8190a3be9aaaf3a7476b18b8930501caa
Vili2103 <116744545+Vili2103@users.noreply.github.com>
2022-12-04 00:12:09 +0200

    Update shoot.cs
    otvrashtava me che ne mahash start() i update() kato ne gi polzvash.

--- shoot.cs
+++ shoot.cs
@@ -7,13 +7,7 @@ public class shoot : MonoBehaviour
 {
     public Transform shootingPoint;
     public GameObject bulletPrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+   
     void Update()
     {
         if(Input.GetMouseButton(0) || Keyboard.current.spaceKey.wasPressedThisFrame) {
