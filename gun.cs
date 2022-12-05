daedc4cd58582d76d15e0e5d0bebd2550287d3f9
Vili2103 <116744545+Vili2103@users.noreply.github.com>
2022-12-05 17:08:42 +0200

    Update gun.cs

--- gun.cs
+++ gun.cs
@@ -30,7 +30,7 @@ public class gun : MonoBehaviour
     }
 
     float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
-         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg-90f;
      }
 
 
