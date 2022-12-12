1b6ed3752e66c564a1a645716afc866dcb38f507
Vilian Iliev <116744545+Vili2103@users.noreply.github.com>
2022-12-12 09:51:15 +0200

    Update shoot.cs

--- Combat Scripts/shoot.cs
+++ Combat Scripts/shoot.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class shoot : MonoBehaviour
 {
+     [SerializeField]
+    protected WeaponParameters weaponParams;
+    
     public Transform shootingPoint;
-    [SerializeField]
-    private GameObject bulletPrefab;
-    [SerializeField] // Makes this visible in the inspector, even though it's private
-    private float coolDown = 0.0f;
-    coolDown*=50;
+    weaponParams.coolDown*=50;
     private int recharge = 0;
    
    
     void FixedUpdate() // FixedUpdate updates 50 times a second.
     {
-           coolDown++; // this may be dumb and very inefficient but it's the only thing i could think of. 
-        if(Input.GetInput("Fire1") && recharge => coolDown){
-            Instantiate(bulletPrefab,shootingPoint.position, transform.rotation);
+           weaponParams.coolDown++; // this may be dumb and very inefficient but it's the only thing i could think of. 
+        if(Input.GetInput("Fire1") && recharge => weaponParams.coolDown){
+            Instantiate(weaponParams.bulletPrefab,shootingPoint.position, transform.rotation);
         }
     }
 }
