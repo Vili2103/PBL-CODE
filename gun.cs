4903c6132d20115d69eb4f5fba82c372fbd7ee3d
Victor Ivanov <80967320+Vic2rious@users.noreply.github.com>
2023-04-26 22:15:37 +0300

    GunThatWorks
    Didn't know which folder to use, sorry

---
+++ gun.cs
@@ -0,0 +1,30 @@
+// object's sprite must have pivot point set to bottom in order to work
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //Debug.Log("Hit!");
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+       var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+       transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+
+    }
+
+
+
+}
